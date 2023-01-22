a8f4713cd86677f7bcc22305a32135c433de0b0f
gralmeidan <gralmeidan@gmail.com>
2023-01-22 10:12:33 -0400

    chore(back): added config for nyc

---
+++ app/back/nyc.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  all: true,
+  exclude: [
+    'src/tests',
+    'src/database/config',
+    'src/database/migrations',
+    'src/database/seeders',
+    'src/server.ts',
+    'src/types',
+  ],
+  include: ['src/**/*.ts'],
+};
