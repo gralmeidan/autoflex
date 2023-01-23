8adbef0602dcab43d369783435dd7371f76b6bee
gralmeidan <gralmeidan@gmail.com>
2023-01-23 18:15:52 -0400

    feat(front): added service class for /materials and made ItemsListPage's fetch function generic

--- app/front/src/pages/ItemsList/index.tsx
+++ app/front/src/pages/ItemsList/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import productService from '../../services/product.service';
 import { type Material } from '../../types/materials.types';
 import { type ProductByFindAll } from '../../types/products.types';
+import productService from '../../services/product.service';
+import materialService from '../../services/material.service';
 
 export default function ItemsListPage() {
   const [data, setData] = useState([] as ProductByFindAll[] | Material[]);
@@ -10,7 +11,9 @@ export default function ItemsListPage() {
 
   useEffect(() => {
     (async () => {
-      const resp = await productService.fetchAll();
+      const service =
+        pathname === '/products' ? productService : materialService;
+      const resp = await service.fetchAll();
       setData(resp);
     })();
   }, [setData]);
---
+++ app/front/src/services/material.service.ts
@@ -0,0 +1,12 @@
+import { type Material } from '../types/materials.types';
+import ItemService from './item.service';
+
+class MaterialService extends ItemService<Material, Material[]> {
+  constructor() {
+    super('materials');
+  }
+}
+
+// Seems redundant but we're declaring the variable first for intellisense
+const materialService = new MaterialService();
+export default materialService;
