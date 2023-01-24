78d9e07432b1bd0f1b797ce5899c9cc1f1be394a
gralmeidan <gralmeidan@gmail.com>
2023-01-24 14:02:31 -0400

    feat(front): added checkbox to toggle uncraftable items

--- app/front/src/pages/ItemsList/index.tsx
+++ app/front/src/pages/ItemsList/index.tsx
@@ -8,20 +8,33 @@ import ItemTable from './ItemTable';
 
 export default function ItemsListPage() {
   const [data, setData] = useState([] as ProductByFindAll[] | Material[]);
+  const [includeUncraftable, setIncludeUncraftable] = useState(false);
   const { pathname } = useLocation();
-
   useEffect(() => {
     (async () => {
       const service =
         pathname === '/products' ? productService : materialService;
-      const resp = await service.fetchAll();
+      const resp = await service.fetchAll({ includeUncraftable });
       setData(resp);
     })();
-  }, [setData, pathname]);
+  }, [setData, pathname, includeUncraftable]);
 
   return (
-    <div className="container py-8">
+    <main className="container py-8">
+      <div className="flex justify-between w-full items-center py-3">
+        <button className="w-fit px-4 py-2 pl-3">+ Novo</button>
+        <label className="select-none ">
+          <input
+            type="checkbox"
+            checked={includeUncraftable}
+            onChange={({ target: { checked } }) => {
+              setIncludeUncraftable(checked);
+            }}
+          />
+          Incluir não criáveis
+        </label>
+      </div>
       <ItemTable data={data} />
-    </div>
+    </main>
   );
 }
--- app/front/src/services/item.service.ts
+++ app/front/src/services/item.service.ts
@@ -7,13 +7,25 @@ export default abstract class ItemService<FindOneType, FindAllType> {
     this.baseUrl = `http://localhost:3001/${type}`;
   }
 
-  public async fetchAll(): Promise<FindAllType> {
+  public async fetchAll(
+    query?: Record<string, string | number | boolean>,
+  ): Promise<FindAllType> {
     return axios
-      .get(`${this.baseUrl}/`)
+      .get(`${this.baseUrl}?${this.encodeQueryObj(query)}`)
       .then(({ data }) => data as FindAllType);
   }
 
   public async fetchOne(id: number | string): Promise<FindOneType> {
     return axios.get(`${this.baseUrl}/${id}`);
+  }
+
+  protected encodeQueryObj(
+    query: Record<string, string | number | boolean> = {},
+  ): string {
+    return Object.keys(query)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(query[key]),
+      )
+      .join(',');
   }
 }
