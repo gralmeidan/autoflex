3a36daccaab77c74c6c980b191cf83212a81fd36
gralmeidan <gralmeidan@gmail.com>
2023-01-26 14:20:38 -0400

    feat(front): added google's material symbols font

--- app/front/src/components/SmallItemCard.tsx
+++ app/front/src/components/SmallItemCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 
 export default function SmallItemCard({
   name,
@@ -6,12 +7,29 @@ export default function SmallItemCard({
   info: { quantity: required },
 }: SmallItemCardProps) {
   return (
-    <li className="ul-card">
-      <p>{name}</p>
-      <abbr title="Necessário/Estoque" style={{ borderBottom: 0 }}>
-        {required}
-        {typeof stock !== 'undefined' && `/${stock}`}
-      </abbr>
+    <li
+      className="ul-card flex justify-between items-start list-none p-2
+     border-[#18232c] border-2 rounded-md flex-grow gap-3"
+    >
+      <div>
+        <p>{name}</p>
+        <abbr title="Necessário/Estoque" style={{ borderBottom: 0 }}>
+          {required}
+          {typeof stock !== 'undefined' && `/${stock}`}
+        </abbr>
+      </div>
+      <div className="flex flex-col justify-between gap-2 h-full">
+        <Button
+          label="delete"
+          onClick={console.log}
+          className="material-symbols-outlined"
+        />
+        <Button
+          label="edit"
+          onClick={console.log}
+          className="material-symbols-outlined"
+        />
+      </div>
     </li>
   );
 }
--- app/front/src/index.tsx
+++ app/front/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/pico.min.css';
+import './styles/material-symbols.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
